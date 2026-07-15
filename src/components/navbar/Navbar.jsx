60737314c374b035b4b7b264d4f9a1e0287ffbbe
islomjonbositov1-ai <islomjonbosiov1@g,ail.com>
2026-07-15 15:10:22 +0500

    Changed header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,31 @@
+import "./Navbar.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <h1>Store</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link>About</Link>
+          </li>
+          <li>
+            <Link>Services</Link>
+          </li>
+          <li>
+            <Link>Contacts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="btns">
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
