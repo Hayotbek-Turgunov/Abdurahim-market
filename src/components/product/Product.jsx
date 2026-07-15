05cb24e3c7bca85437f71acb786d6bc5f07db213
Muhammadali <22082012muhammadali@gmail.com>
2026-07-15 15:10:26 +0500

    kskjgfjh

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,5 +1,10 @@
 const Product = () => {
-  return <div>Product</div>;
+  return(
+    <div>
+      <img src="https://www.ubuy.uz/ru/productde/PH51UVQP6-apple-iphone-16-pro-512gb-titanium-black" alt="" />
+      <h1>Iphone 16 Pro</h1>
+    </div>
+  )  
 };
 
 export default Product;
